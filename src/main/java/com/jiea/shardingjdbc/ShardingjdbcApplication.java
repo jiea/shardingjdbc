813bed929ac92230df3f3d6542c5ed72503b1455
李杰 <jie.li@ly.com>
2018-11-05 22:31:23 +0800

    first commit

---
+++ src/main/java/com/jiea/shardingjdbc/ShardingjdbcApplication.java
@@ -0,0 +1,12 @@
+package com.jiea.shardingjdbc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShardingjdbcApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ShardingjdbcApplication.class, args);
+    }
+}
